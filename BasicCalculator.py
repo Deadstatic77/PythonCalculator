114fd13a42a4b7062fe62fca5b073c0659c5173b
Deadstatic77 <106870522+Deadstatic77@users.noreply.github.com>
2022-06-05 00:42:52 +1000

    Update BasicCalculator.py

--- BasicCalculator.py
+++ BasicCalculator.py
@@ -1,29 +1,32 @@
-O = int(input("Enter first number: "))
-T = int(input("Enter second number: "))
-W = input("\nAddition = add\nSubtraction = sub\nMultiplication = mult\nDivision = div\n")
+def main():
+    O = int(input("Enter first number: "))
+    T = int(input("Enter second number: "))
+    W = input("\nAddition = add\nSubtraction = sub\nMultiplication = mult\nDivision = div\n")
+
+    def subtraction():
+        print(O - T, "\n")
+
+    def addition():
+        print(O + T, "\n")
+
+    def multiplication():
+        print(O * T, "\n")
+
+    def division():
+        print(O / T, "\n")
+
+    if W == "sub":
+        subtraction()
+        main()
+    elif W == "add":
+        addition()
+        main()
+    elif W == "mult":
+        multiplication()
+        main()
+    elif W == "div":
+        division()
+        main()
 
 
-def subtraction():
-    print(O - T)
-
-
-def addition():
-    print(O + T)
-
-
-def multiplication():
-    print(O * T)
-
-
-def division():
-    print(O / T)
-
-
-if W == "sub":
-    subtraction()
-elif W == "add":
-    addition()
-elif W == "mult":
-    multiplication()
-elif W == "div":
-    division()
+main()
